ce86cfa7f6eb1b75ce1a03f7039543703bae0a03
CapB1ack <capback250@gmail.com>
2018-01-16 22:22:01 +0300

    logic

--- client/app/components/percent/percent.controller.js
+++ client/app/components/percent/percent.controller.js
@@ -1,26 +1,46 @@
 class PercentController {
-  constructor() {
-    this.data = [
-        {name:'xx', percent: 0},
-        {name:'yy', percent: 0}
-    ];
-    this.data_copy = [
-        {name:'xx', percent: 0},
-        {name:'yy', percent: 0}
-    ];
-    this.handleChange = (e) => {
-        this.data_copy[e].percent = this.data[e].percent;
-    };
+    constructor() {
+        'ngInject';
 
-    this.handleChangeInput = (e) => {
-      console.log(this.data_copy[e].percent);
-      if (!this.data_copy[e].percent) {
-          this.data_copy[e].percent = this.data[e].percent;
-      } else {
-          this.data[e].percent = this.data_copy[e].percent;
-      }
+        this.data = [{"Name": "Item 1", "Percent": 0}, {"Name": "Item 2", "Percent": 0}, {
+            "Name": "Item 3",
+            "Percent": 0
+        }];
+        this.checkSummary();
+        this.old = angular.copy(this.data);
+        this.amount = void 0;
+        this.total = void 0;
     }
-  }
+
+    sliderDown (index) {
+        this.amount = this.data[index]['Percent'];
+    }
+
+    slider (index) {
+        const delta = this.data[index]['Percent'] - (this.amount === void 0 ? this.old[index]['Percent'] : this.amount);
+        if(delta < 0) {
+            this.smaller(index, delta)
+        }
+    }
+
+    smaller (index, delta) {
+        const smallerVal = Math.min(...this.data.filter((_, i)=> i !== index).map(e=> e['Percent']));
+        const indexSmallest = this.data.findIndex(e=> e['Percent'] === smallerVal);
+        this.data[indexSmallest]['Percent'] += Math.abs(delta);
+        this.amount += delta;
+        this.total = this.data.reduce((sum, cur) => sum += cur['Percent'], 0)
+    }
+
+    checkSummary() {
+        let delta = 100 - this.data.reduce((sum, cur) => sum += cur['Percent'], 0);
+        let i = 0;
+        while (delta > 0) {
+            const elDelta = 100 - this.data[i]['Percent'];
+            this.data[i]['Percent'] += elDelta;
+            delta -= elDelta;
+            i++;
+        }
+    }
 }
 
 export default PercentController;
